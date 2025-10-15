6ac34d3950119453360ab87f16612a4febd483f0
Mudassir Iqbal <mudassir.iqbal0999@gmail.com>
2025-10-15 13:11:06 +0500

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -19,7 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    "http://localhost:5173",
+    "https://techmobiles-frontend.vercel.app"
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
