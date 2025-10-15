363704f38f41bb4ba19791237f8df8ec7ae7f211
Mudassir Iqbal <mudassir.iqbal0999@gmail.com>
2025-10-15 08:40:39 +0500

    changes done

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,37 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const productRoutes = require('../routes/products.route');
-const userRoutes = require("../routes/user.route")
-const orderRoutes = require("../routes/order.route");
-const dashboardRouter = require('../routes/admin/dashboard.route');
+const serverless = require('serverless-http');
+const app = require('../index');
 
-
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-
-
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
-
-
-app.use('/api/products', productRoutes);
-app.use('/api/user', userRoutes );
-app.use('/api/order', orderRoutes );
-
-app.use('/api/admin', dashboardRouter );
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(process.env.PORT)
-}).catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-});
+module.exports = serverless(app)
